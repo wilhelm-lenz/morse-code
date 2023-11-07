9c53215eed024a21853ed5ffe807ecfa8ca7483c
wilhelm-lenz <wilhelm.v.lenz@gmail.com>
2023-11-07 12:25:08 +0100

    completed project at the moment

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,69 @@
-"use strict"
+"use strict";
+
+let morseAlphabet = [
+  { letter: "1", morseCode: ".----" },
+  { letter: "2", morseCode: "..---" },
+  { letter: "3", morseCode: "...--" },
+  { letter: "4", morseCode: "....-" },
+  { letter: "5", morseCode: "....." },
+  { letter: "6", morseCode: "-...." },
+  { letter: "7", morseCode: "--..." },
+  { letter: "8", morseCode: "---.." },
+  { letter: "9", morseCode: "----." },
+  { letter: "0", morseCode: "-----" },
+  { letter: " ", morseCode: "    " },
+  { letter: "A", morseCode: ".-" },
+  { letter: "B", morseCode: "-..." },
+  { letter: "C", morseCode: "-.-." },
+  { letter: "D", morseCode: "-.." },
+  { letter: "E", morseCode: "." },
+  { letter: "F", morseCode: "..-." },
+  { letter: "G", morseCode: "--." },
+  { letter: "H", morseCode: "...." },
+  { letter: "I", morseCode: ".." },
+  { letter: "J", morseCode: ".---" },
+  { letter: "K", morseCode: "-.-" },
+  { letter: "L", morseCode: ".-.." },
+  { letter: "M", morseCode: "--" },
+  { letter: "N", morseCode: "-." },
+  { letter: "O", morseCode: "---" },
+  { letter: "P", morseCode: ".--." },
+  { letter: "Q", morseCode: "--.-" },
+  { letter: "R", morseCode: ".-." },
+  { letter: "S", morseCode: "..." },
+  { letter: "T", morseCode: "-" },
+  { letter: "U", morseCode: "..-" },
+  { letter: "V", morseCode: "...-" },
+  { letter: "W", morseCode: ".--" },
+  { letter: "X", morseCode: "-..-" },
+  { letter: "Y", morseCode: "-.--" },
+  { letter: "Z", morseCode: "--.." },
+];
+
+const textInput = document.querySelector("#text-for-encrypting");
+const outputText = document.querySelector(".output");
+
+console.log(textInput);
+console.log(outputText);
+
+const textToMorseCode = () => {
+  event.preventDefault();
+  let inputText = textInput.value;
+  let output = "";
+  for (let i = 0; i < inputText.length; i++) {
+    for (let j = 0; j < morseAlphabet.length; j++) {
+      if (
+        inputText.charAt(i).toLowerCase() ===
+          morseAlphabet[j].letter.toLowerCase() &&
+        inputText.charAt(i) !== " "
+      ) {
+        output += morseAlphabet[j].morseCode;
+        console.log(outputText);
+      }
+    }
+    if (inputText.charAt(i) === " ") {
+      output += "/";
+    }
+  }
+  outputText.textContent = output;
+};
